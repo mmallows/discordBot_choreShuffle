09a2420bcb841ecfe278ab47807c94eeb7d35c5b
Marcus Mallows <marcusmallows@aol.com>
2023-03-14 15:27:40 -0400

    added comments, cleaned code of bad variables

--- main.py
+++ main.py
@@ -7,53 +7,77 @@ import keep_alive
 client = discord.Client()
 oldChores = [-2, -2, -2, -2, -2]
 
-def shuffleChores():
-  chores = ["vacuum", "kitchen", "upstairs bathroom", "downstairs bathroom", "sweep/mop & living room"]
-  people = ["Andy", "Adrien", "Jeson", "Justin", "Marcus"]
-  newChores = [-1, -1, -1, -1, -1]
-  currentDate = datetime.datetime.now()
-  str0 = "Weekly Chores: " + str(currentDate.month) + "/" + str(currentDate.day) + "\n"
-  
-  def roll(x):
-    rand = random.randrange(0, 5-x)
-    while(x < 4 and rand == oldChores[x]):
-        rand = random.randrange(0, 5-x)
-    return rand
-  
-  for i in range(5):
-    nextChore = roll(i)
-    if (i == 4 and nextChore == oldChores[4]):
-      rand = random.randrange(4)
-      temp = nextChore
-      nextChore = newChores[rand]
-      newChores[rand] = temp
-    newChores[i] = nextChore
-    str0 += people[i] + ": " + chores[nextChore] + "\n"
-    del chores[nextChore]
-  return str0
-    
-    
+
+def shuffleChores():  # method to shuffle chores: returns string with list of chores assigned to roommates
+    chores = [
+        "vacuum",
+        "kitchen",
+        "upstairs bathroom",
+        "downstairs bathroom",
+        "sweep/mop & living room",
+    ]  # list of chores to choose from, should be same size as people list
+    people = [
+        "Andy",
+        "Adrien",
+        "Jeson",
+        "Justin",
+        "Marcus",
+    ]  # define list of roommates to split chores between
+    newChores = [-1, -1, -1, -1, -1]
+    currentDate = datetime.datetime.now()
+    returnString = (
+        "Weekly Chores: " + str(currentDate.month) + "/" + str(currentDate.day) + "\n"
+    )
+
+    def roll(chore_index):  # returns randomly generated index for next chore
+        nextChore_index = random.randrange(0, len(chores))
+        while (
+            chore_index < 4 and nextChore_index == oldChores[chore_index]
+        ):  # attempts to ensure new chores are different from old chores
+            # first condition (chore_index < 4) keeps method from looping forever on selecting last chore if same as old chore
+            nextChore_index = random.randrange(0, len(chores))
+        return nextChore_index
+
+    totalChores = len(chores)
+    for chore_index in range(totalChores):
+        nextChore_index = roll(chore_index)
+        if (
+            chore_index == 4 and nextChore_index == oldChores[4]
+        ):  # if last job is same as before, swap with random other person
+            swap_index = random.randrange(4)
+            nextChore_index, newChores[swap_index] = (
+                newChores[swap_index],
+                nextChore_index,
+            )
+        newChores[chore_index] = nextChore_index
+        returnString += people[chore_index] + ": " + chores[nextChore_index] + "\n"
+        del chores[nextChore_index]
+        oldChores[chore_index] = nextChore_index
+    return returnString
+
 
 @client.event
 async def on_ready():
-  print("Ready. Logged in as: {0.user}".format(client))
+    print("Ready. Logged in as: {0.user}".format(client))
+
 
 @client.event
-async def on_message(message): #Message Response:
-  if message.author == client.user:
-    return
-  if message.content.startswith('$'): #All User Response:
-    if message.content == "$ hello":
-      await message.channel.send("Hello, {0.author.name}!".format(message))
-      await message.add_reaction("\U0001F596")
-    if message.author.id == int(os.environ['OWNER']): #Owner Response:
-      if message.content == "$ shuffle chores":
-        string = shuffleChores()
-        await message.channel.send(string)
-    else:
-      print("Message not from owner.")
-      print(message.content + "\n" + "ID: " + str(message.author.id))
-      await message.channel.send("Who are you, my mother?")
+async def on_message(message):  # Message Response:
+    if message.author == client.user:
+        return
+    if message.content.startswith("$"):  # All User Response:
+        if message.content == "$ hello":
+            await message.channel.send("Hello, {0.author.name}!".format(message))
+            await message.add_reaction("\U0001F596")
+        if message.author.id == int(os.environ["OWNER"]):  # Owner Response:
+            if message.content == "$ shuffle chores":
+                string = shuffleChores()
+                await message.channel.send(string)
+        else:  # -- Response for anything starting with $ but not from owner
+            print("Message not from owner.")
+            print(message.content + "\n" + "ID: " + str(message.author.id))
+            await message.channel.send("Who are you, my mother?")
+
 
 keep_alive.keep_alive()
-client.run(os.environ['TOKEN'])
+client.run(os.environ["TOKEN"])
